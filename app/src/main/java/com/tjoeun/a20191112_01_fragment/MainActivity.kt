b5f1d7eb2121739fad137e9a9f9eeeffab9bc680
sangone88 <llil1@naver.com>
2019-11-12 19:43:32 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191112_01_fragment/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.tjoeun.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        firstTxt.text = "첫 테스트"
+        secondTxt.text = "두번째 테스트"
+
+    }
+
+
+}
