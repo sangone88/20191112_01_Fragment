e35a7116ce3670db33a0d0dc08e3cfe0754b5952
sangone88 <llil1@naver.com>
2019-11-12 19:49:28 +0900

    첫 프래그먼트 생성

---
+++ app/src/main/java/com/tjoeun/a20191112_01_fragment/fragment/FirstFragment.kt
@@ -0,0 +1,19 @@
+package com.tjoeun.a20191112_01_fragment.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tjoeun.a20191112_01_fragment.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+}
